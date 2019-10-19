e5691544314c50c9df68f6dfe8d023b84a515dc6
Thiago Santos <thiago.santos@moneyex.io>
2019-10-19 11:39:25 -0300

    Added ktlin and detekt configurations

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import com.vanniktech.android.junit.jacoco.JunitJacocoExtension
+import configs.KotlinConfig
+import io.gitlab.arturbosch.detekt.detekt
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 buildscript {
     repositories {
@@ -22,8 +25,24 @@ allprojects {
         google()
         jcenter()
     }
+
+    apply(plugin = PluginIds.DETEKT)
+
+    detekt {
+        config = files("$rootDir/default-detekt-config.yml")
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = KotlinConfig.targetJVM
+    }
 }
 
 tasks.register("clean").configure {
     delete(rootProject.buildDir)
+}
+
+apply(plugin = PluginIds.JACOCO_UNIFIED)
+
+configure<JunitJacocoExtension> {
+    jacocoVersion = "0.8.4"
 }
