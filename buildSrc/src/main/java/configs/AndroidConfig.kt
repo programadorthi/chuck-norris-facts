da5e36c47f16387371e3f2092279df9551f99aad
Thiago Santos <programadorthi@gmail.com>
2020-10-31 14:48:29 -0300

    upgrade to Android R

--- buildSrc/src/main/java/configs/AndroidConfig.kt
+++ buildSrc/src/main/java/configs/AndroidConfig.kt
@@ -6,11 +6,11 @@ import java.util.Arrays
 object AndroidConfig {
     const val APPLICATION_ID = "br.com.programadorthi.chucknorrisfacts"
 
-    const val COMPILE_SDK_VERSION = 29
-    const val MIN_SDK_VERSION = 21
+    const val COMPILE_SDK_VERSION = 30
+    const val MIN_SDK_VERSION = 23
     const val TARGET_SDK_VERSION = COMPILE_SDK_VERSION
 
-    const val BUILD_TOOLS_VERSION = "29.0.2"
+    const val BUILD_TOOLS_VERSION = "30.0.2"
 
     const val INSTRUMENTATION_TEST_RUNNER = "androidx.test.runner.AndroidJUnitRunner"
 
